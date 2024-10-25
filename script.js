86f5997b292ac79b9f75b9cba736a251f0371fa4
natalyoliveir <nataly.de.oliveira@escola.pr.gov.br>
2024-10-25 12:23:03 -0300

    atualizar

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+Function mostra resultado()
+{
+    Var numero 1=10;
+    Var numero 2=15
+    Var resultado= numero1+numero2;
+    document.getElementById
+    ('resultado'). innerHTML=
+    resultado
 
